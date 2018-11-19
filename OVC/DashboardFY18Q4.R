b432e30618f55e9fa1475e1694f070411e8c0394
Katya <shapaklyak@users.noreply.github.com>
2018-11-19 13:52:33 -0500

    Update DashboardFY18Q4.R
    Updated to remove Exited without Graduation from ProgramStatus disagg since it appears under both ProgramStatus & ExitTransfer

--- OVC/DashboardFY18Q4.R
+++ OVC/DashboardFY18Q4.R
@@ -1,9 +1,12 @@
-#Date updated: 11/16/2018
+#Date updated: 11/19/2018
 #Purpose: To subset PSNU X IM Genie Extract for OVC FY18Q4 clean dashboard
 #Software: R
 
-#increase memory
 memory.limit(size = 90000)
+
+
+
+getwd()
 
 #install & load packages 
 install.packages('tidyverse')
@@ -15,10 +18,11 @@ library(skimr)
 library(tidyverse)
 library(ICPIutilities)
 
-#read genie file using ICPI Utilities (read_msd)
-genie <- read_msd("DHIS2_ef2d178f5bd340779acfcea01eb19bed.txt")
+#read genie file
+genie <- read_msd("Genie Download OVC_SERV OVCHIVSTAT_20181116.txt")
 
 #list column names and numbers
+
 colnames(genie)
 
 #drop any unnecessary columns for OVC dashboard
@@ -31,59 +35,56 @@ str(genie1)
 names(genie1)
 unique(genie1$standardizeddisaggregate)
 
-#remove age/sex/service from the standardized disaggregate column since that is only for DREAMS 
+#remove age/sex/service  
 genie2 <- genie1[ which(genie1$standardizeddisaggregate !='Age/Sex/Service'), ]
 names(genie2)
 unique(genie2$standardizeddisaggregate)
 
-#check if the dataset contains any NAs
-any(is.na(genie2))
+#remove exit without graduation from program status as it is duplicated under ProgramStatus and ExitTransfer
+genie3 <- filter(genie2, standardizeddisaggregate != "ProgramStatus" | categoryoptioncomboname != "Exited without Graduation")
+names(genie3)
+unique(genie3$standardizeddisaggregate)
+unique(genie3$categoryoptioncomboname)
+
+#does the dataset contain any NAs?
+any(is.na(genie3))
 
 #how many NAs are there?
-sum(is.na(genie2))
+sum(is.na(genie3))
 
 #which columns have NAs and how many?
-colSums(is.na(genie2))
+colSums(is.na(genie3))
 
 #check the struture and stats for a specific set of columns
-genie2 %>% 
+genie3 %>% 
   select(25:33) %>% 
   skim()
 
-#create a new dataset to filter out NAs & zeroes
-genie3 <- genie2
+#new dataset to filter out NAs & zeroes
+genie4 <- genie3
 
-#look at how many records would be left without NAs and zeroes across FY columns
-genie3 %>% 
+# look at how many records would be left without NAs and zeroes across FY columns
+genie4 %>% 
   filter_at(vars(starts_with("FY")), any_vars(!is.na(.) & .!=0)) %>% 
   nrow()
 
-#filter out the NAs & zeroes
-genie4 <- genie3 %>% 
+#  filter out the NAs & zeroes
+genie5 <- genie4 %>% 
   filter_at(vars(starts_with("FY")), any_vars(!is.na(.) & .!=0))
 
 #how many records remain after removing NAs and zeroes
-nrow(genie3) - nrow(genie4)
+nrow(genie4) - nrow(genie5)
 
 #check the struture and stats for a specific set of columns
-genie4 %>% 
+genie5 %>% 
   select(standardizeddisaggregate) %>% 
   skim()
 
 
-#move "exited without graduated" from program status StandardizedDisaggregate to Transferexit since it is duplicated in the ProgramStatus category
-genie5 <- genie4 %>%
-  mutate(standardizeddisaggregate = case_when(
-    otherdisaggregate %in% c('Exited without Graduation', 'Transferred')~ 'TransferExit',
-    TRUE~standardizeddisaggregate))
-
-#check dataset
-head(genie5)
-
 # Create new column called "calcfy2018apr" to pull Q4 values from Age/Sex disaggs and TransferExit disaggs into new calcfy2018aapr column, all other values take from fy2018apr and put into new "calcfy2018apr" column. 
 genie6 <- mutate(genie5, calcfy2018apr = if_else(indicator == "OVC_SERV" & standardizeddisaggregate == "Age/Sex", fy2018q4,
-                                       if_else(indicator == "OVC_SERV" & standardizeddisaggregate == "TransferExit", fy2018q4, fy2018apr)))
+                                                 if_else(indicator == "OVC_SERV" & standardizeddisaggregate == "TransferExit", fy2018q4, fy2018apr)))
 
 
 #export wide  to csv/txt for dashboard (testing age/sex and transferexit APR calculation)
-write.csv(genie6,"GeniePSNUxIM_OVC_20181116b.csv", row.names = FALSE)
+write.csv(genie6,"GeniePSNUxIM_OVC_20181119fix.csv", row.names = FALSE)
