840a60264f5157bc4fe4802842284f35514bcd06
Katya <shapaklyak@users.noreply.github.com>
2018-11-20 11:58:31 -0500

    Update DashboardFY18Q4.R
    Updated code to account for issue in DATIM APR values for OVC_HIVSTAT.

--- OVC/DashboardFY18Q4.R
+++ OVC/DashboardFY18Q4.R
@@ -1,9 +1,8 @@
-#Date updated: 11/19/2018
+#Date updated: 11/20/2018
 #Purpose: To subset PSNU X IM Genie Extract for OVC FY18Q4 clean dashboard
 #Software: R
 
 memory.limit(size = 90000)
-
 
 
 getwd()
@@ -28,7 +27,7 @@ colnames(genie)
 #drop any unnecessary columns for OVC dashboard
 genie1 <- genie %>% 
   select(operatingunit, countryname:snu1, psnu, snuprioritization:typemilitary, primepartner:implementingmechanismname, indicator:standardizeddisaggregate,
-         categoryoptioncomboname:coarsedisaggregate, fy2017_targets, fy2017q2, fy2017q4:fy2017apr, fy2018_targets, fy2018q2, fy2018q4:fy2019_targets)
+         categoryoptioncomboname:coarsedisaggregate, fy2018_targets, fy2018q2, fy2018q4:fy2019_targets)
         
 #check the structure of dataset
 str(genie1)
@@ -40,8 +39,9 @@ genie2 <- genie1[ which(genie1$standardizeddisaggregate !='Age/Sex/Service'), ]
 names(genie2)
 unique(genie2$standardizeddisaggregate)
 
-#remove exit without graduation from program status as it is duplicated under ProgramStatus and ExitTransfer
+#remove exit without graduation from program status
 genie3 <- filter(genie2, standardizeddisaggregate != "ProgramStatus" | categoryoptioncomboname != "Exited without Graduation")
+
 names(genie3)
 unique(genie3$standardizeddisaggregate)
 unique(genie3$categoryoptioncomboname)
@@ -81,10 +81,17 @@ genie5 %>%
   skim()
 
 
+# Check indicators
+unique(genie5$indicator)
+
+
 # Create new column called "calcfy2018apr" to pull Q4 values from Age/Sex disaggs and TransferExit disaggs into new calcfy2018aapr column, all other values take from fy2018apr and put into new "calcfy2018apr" column. 
 genie6 <- mutate(genie5, calcfy2018apr = if_else(indicator == "OVC_SERV" & standardizeddisaggregate == "Age/Sex", fy2018q4,
-                                                 if_else(indicator == "OVC_SERV" & standardizeddisaggregate == "TransferExit", fy2018q4, fy2018apr)))
+                                                 if_else(indicator == "OVC_SERV" & standardizeddisaggregate == "TransferExit", fy2018q4, 
+                                                         if_else(indicator == "OVC_HIVSTAT" | indicator == "OVC_HIVSTAT_POS" | indicator == "OVC_HIVSTAT_NEG", fy2018q4, 
+                                                                 if_else(indicator == "OVC_SERV", fy2018q4, fy2018apr)))))
 
 
 #export wide  to csv/txt for dashboard (testing age/sex and transferexit APR calculation)
-write.csv(genie6,"GeniePSNUxIM_OVC_20181119fix.csv", row.names = FALSE)
+write.csv(genie6,"GeniePSNUxIM_OVC_20181120.csv", row.names = FALSE)
+
