f9814f48219238f8569846ecd7f0e23c6bad37ef
Katya Noykhovich <shapaklyak@users.noreply.github.com>
2019-02-05 17:07:39 -0500

    Update and rename Subset&CorrectMSD.R to DRAFT_SubsetMSD.R
    issues with creating a total num value for FY17 OVC_SERV

--- OVC/OVCTrends/DRAFT_SubsetMSD.R
+++ OVC/OVCTrends/DRAFT_SubsetMSD.R
@@ -18,6 +18,7 @@ install.packages("skimr")
 library(skimr)
 library(tidyverse)
 library(ICPIutilities)
+library(dplyr)
 
 
 #1 read df file
@@ -39,7 +40,6 @@ unique(df2$indicator)
 ##EXPLORATION: what are the unique disaggregates under OVC_SERV?
 testdisaggs <- data.frame(df2$indicator, df2$standardizeddisaggregate, df2$otherdisaggregate)
 unique(testdisaggs[which(testdisaggs$df2.indicator == "OVC_SERV"), ])
-
 
 
 #5 remove age/sex/service disaggregate as that is DREAMS only 
@@ -74,15 +74,28 @@ df6 <- df5 %>%
 #13 how many records remain after removing NAs and zeroes
 nrow(df6) - nrow(df5)
 
-### working draft code below
+#14 reshape to long
+longdf <- gather(df6, key = "period", "value", fy2017_targets:fy2019_targets, na.rm=TRUE)
+write.csv(longdf,"PSNUxIM_OVC_20181221_Feb5.csv", row.names = FALSE)
+
+
+### WORKING DRAFT CODE BELOW 
 ###Create new column called "calcfy17apr" to pull Q4 values from Total Num, all other values take from fy17apr and put into new "calcfy17apr" column. 
-df7 <- mutate(df6, calcfy17apr = if_else(indicator == "OVC_SERV" & standardizeddisaggregate == "Total Numerator", fy2017q4, fy2017apr))
+#df7 <- mutate(df6, calcfy17apr = if_else(indicator == "OVC_SERV" & standardizeddisaggregate == "Total Numerator", fy2017q4, fy2017apr))
+# fix OVC SERV total numerator FY17apr = active fy17apr + grad fy17 apr
 
 # add fy2017apr active + graduated to equal Total Numerator for fy2017apr
-df8 <- df7 %>% 
-  dplyr:mutate(calcfyapr = ifelse(()))
+#df8 <- mutate(df7, calcfyapr = ifelse((otherdisaggregate == "Active" & otherdisaggregate == "Graduated", fy2017apr + fy2017apr ,fy2017apr)
 
+# df8 <- df7 %>% 
+#   group_by(indicator == "OVC_SERV", otherdisaggregate) %>% 
+#   mutate_if(calcfy17apr = ifelse((otherdisaggregate == "Active"), fy2017apr + if(otherdisaggregate == "Graduated", fy2017apr,fy2017apr),fy2017apr))
+# 
+# df8 <- df7 %>% 
+#  group_by(indicator == "OVC_SERV", otherdisaggregate) %>% 
+#   filter(otherdisaggregate == "Active" | "Graduated") %>% 
+#   mutate(calcfy17apr = fy2017apr + fy2017apr)
+# 
 
 #export wide  to csv/txt for dashboard (testing age/sex and transferexit APR calculation)
-write.csv(df7,"PSNUxIM_OVC_20181221_Feb5.csv", row.names = FALSE)
-
+#write.csv(df7,"PSNUxIM_OVC_20181221_Feb5.csv", row.names = FALSE)
