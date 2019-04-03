70974e66f5bbb7094d89122c1122cbffb52c501a
Faith Fawale <44295193+faithfawale@users.noreply.github.com>
2019-04-03 09:05:21 -0400

    Update Calculated Indicator_OVC_Serv Code_ 2version.R

--- OVC/MSDExplorations/Calculated Indicator_OVC_Serv Code_ 2version.R
+++ OVC/MSDExplorations/Calculated Indicator_OVC_Serv Code_ 2version.R
@@ -18,36 +18,40 @@ library(dplyr)
 
 #import data
 Play1 <- read_csv("C:/Users/ovu6/Desktop/Prevention Cluster/OVC/Calculated Indicator/OVC_MSD_PSNUxIM_Play.csv")
+View(OVC_MSD_PSNUxIM_Play)
 
-#creating new column pulling out the total numerator values for OVC_SERV indicator 
+
+#Create new column called "calcfy17apr" to pull Q4 values from Total Num, all other values take from fy17apr and put into new "calcfy17apr" column.  
 Play2 <- mutate(Play1, calcfy17apr = if_else(indicator == "OVC_SERV" & standardizeddisaggregate == "Total Numerator",fy2017q4, fy2017apr)) 
+#fix OVC SERV total numerator FY17apr = active fy17apr + grad fy17 apr 
+colnames(Play2)  
 
 
-Play3 <- Play2 %>% 
-  filter(indicator == "OVC_SERV" & categoryoptioncomboname %in% c("Active", "Graduated")) %>%
-  group_by(psnu,standardizeddisaggregate,indicatortype) %>% 
-  summarise(calcfy17apr = sum(calcfy17apr)) %>%
-  mutate("Total Numerator")
-
-colnames(Play3)
-
-
+#get rid of unwanted columns for FY (anything thats not FY17 )
+Drop <- select(Play2, -starts_with("fy2018"))
+Drop2 <- select(Drop, -starts_with("fy2019"))
+Drop3 <- select(Drop2, -c("fy2017_targets","fy2017q1","fy2017q2","fy2017q3"))
 
 
 
-#*this part of the code is still a working progress
-#reanme column 
-Play4 <-Play3 %>%  rename("standardizeddisaggregate" = "\"Total Numerator\"")
+# to view if where indicator is OVC_SERV is where disagg is total numerator
+# Check <- filter(Drop3, indicator == "OVC_SERV" & standardizeddisaggregate == "Total Numerator")
 
-drop1 <- Play4[, -2]
+#working progress 
+psnu_int <- Drop3 %>% group_by("psnu","indicator") %>% filter((indicator == "OVC_SERV") & (categoryoptioncomboname == "Active" & categoryoptioncomboname == "Graduated"))
+psnu <- unique(Drop3$psnu)
 
-colnames(drop1)
-
-
-#merge to original dataset. exclude calcfy17apr because it is the one being merged to the orginal dataset 
-
-Play5 <- merge( Play1, drop1, by = c("psnu","standardizeddisaggregate", "indicatortype"))
-
+for (n in psnu){
+  active_value <- if_else(exists(Drop3[ which((Drop3$psnu == n) & 
+                                         (Drop3$indicator=="OVC_SERV") & 
+                                         (Drop3$standardizeddisaggregate == "ProgramStatus") & 
+                                         (Drop3$categoryoptioncomboname== "Active")), Drop3$calcfy17apr], 
+                                 Drop3[ which((Play2$psnu == n) & 
+                                         (Drop3$indicator=="OVC_SERV") & 
+                                         (Drop3$standardizeddisaggregate == "ProgramStatus") & 
+                                         (Drop3$categoryoptioncomboname== "Active")), Drop3$calcfy17apr], NA))
+  print(active_value)
+}
 
 
 
